0a99dae27f9f27bb3a202ec292ae3a082fecaca5
ADSbinario <38841266+ADSbinario@users.noreply.github.com>
2023-05-18 17:10:49 -0300

    Add files via upload

---
+++ OpenPages.py
@@ -0,0 +1,18 @@
+import time
+import webbrowser
+
+# Para pentest que utilizam windows, esse programa facilita na hora da listagem de vários
+#subdominios e você ficar abrindo página por página, basta inserir uma lista no arquivo texto.txt
+# e os dominios serão abertos um por um
+with open("texto") as f:
+  content = f.readlines()
+  print(f)
+
+#print(content)
+content = [x.strip('\n') for x in content]
+#print(content)
+
+for line in content:
+    webbrowser.open(line)
+    time.sleep(1)
+    print(line)
